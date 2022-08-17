93704d82219f3d1830cdc7fd1cbcf66bb5a6ba3c
MehrabRahman <mehrab.rahman@revature.com>
2022-08-17 10:56:28 -0700

    Attempting multiple ways of input

--- App.java
+++ App.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
 class App {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        // Check for commandline args
+        if (args.length > 0) {
+            // App -n NAME
+            for (int i = 0; i < args.length; i++) {
+                switch (args[i]) {
+                    case "--name":
+                    case "-n":
+                        System.out.println("Hello, " + args[i+1] +"!");
+                        break;
+                
+                    default:
+                        System.out.println("Unknown arg: " + args[i] + "\nUsage: App [-n] Name");
+                        break;
+                }
+            }
+        } else {
+            // If no args, then check stdin
+            Scanner scanner = new Scanner(System.in);
+            if (scanner.hasNext()) {
+                String name = scanner.nextLine();
+                System.out.println("Hello, " + name + "!");
+            } else {
+                // If no args and no stdin
+                System.out.println("Hello, World!");
+            }
+            scanner.close();
+        }
     }
 }
