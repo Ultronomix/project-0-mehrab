acc562525eaf536c4e9c5e1927bc37b85e59342f
MehrabRahman <mehrab.rahman@revature.com>
2022-08-18 10:01:31 -0700

    Refactoring App.java

--- App.java
+++ App.java
@@ -1,33 +1,53 @@
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 class App {
     public static void main(String[] args) {
+        // Define variables
+        final String GREETING = "Hello";
+        Map<String, Boolean> options = new HashMap<>();
+        options.put("interactive", false);
+        options.put("stdin", false);
+        String name = "World";
+
+        // Building Application's context
         // Check for commandline args
+        name = parseArgs(args, name, options);
+
+        // Responding to the user
+        System.out.println(GREETING + ", " + name + "!");
+    }
+
+    public static String parseArgs(String[] args, String name, Map<String, Boolean> options) {
         if (args.length > 0) {
-            // App -n NAME
+            // Loop through and set up flags
             for (int i = 0; i < args.length; i++) {
                 switch (args[i]) {
                     case "--name":
                     case "-n":
-                        System.out.println("Hello, " + args[i+1] +"!");
+                        name = args[i+1];
+                        i++;
                         break;
-                
+                    case "--interactive":
+                    case "-i":
+                        options.replace("interactive", true);
+                        // Interactive mode
+                        break;
+                    case "--stdin":
+                    case "-s":
+                        options.replace("stdin", true);
+                        Scanner scanner = new Scanner(System.in);
+                        if (scanner.hasNext()) {
+                            name = scanner.nextLine();
+                        }
+                        break;
                     default:
                         System.out.println("Unknown arg: " + args[i] + "\nUsage: App [-n] Name");
                         break;
                 }
             }
-        } else {
-            // If no args, then check stdin
-            Scanner scanner = new Scanner(System.in);
-            if (scanner.hasNext()) {
-                String name = scanner.nextLine();
-                System.out.println("Hello, " + name + "!");
-            } else {
-                // If no args and no stdin
-                System.out.println("Hello, World!");
-            }
-            scanner.close();
-        }
+        } 
+        return name;
     }
 }
